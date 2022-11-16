1045104ead1ff9f2f32d238f5a6f8860f5f0121f
Alexander <alexanders.tischner@outlook.com>
2022-11-15 20:03:48 -0700

    set up components

---
+++ src/components/Footer.js
@@ -0,0 +1,31 @@
+
+
+import { Col, Container, Row } from "react-bootstrap"
+
+
+
+
+
+
+
+
+export const Footer = () => {
+    // html for footer
+    return (
+        <footer className="footer">
+            <Container>
+                <Row className="align-items-center">
+                    <Col size={12} sm={6}>
+                        
+                    </Col>
+                    <Col size={12} sm={6} className="text-center text-sm-end">
+                        <div className="social-icon">
+                           
+                        </div>
+                        <p>Thank you for visiting my portfolio. Always welcome to feedback on improvments! All rights reserved by:  <a href="https://github.com/Sebascrab" target="_blank">  Sebastian Tischner</a></p>
+                    </Col>
+                </Row>
+            </Container>
+        </footer>
+    )
+}
