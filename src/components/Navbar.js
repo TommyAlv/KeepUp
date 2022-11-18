469f0c6dd11a5863e78504bb5fe48540ec663f65
TommyAlv <tommyalvarado2@gmail.com>
2022-11-17 19:11:30 -0700

    navbar changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,17 +14,17 @@ export const Nbar = () => {
   return (
     <Navbar bg="light" expand="lg" className="">
       <Container>
-        <Navbar.Brand href="#home">Group Project 3</Navbar.Brand>
+        <Navbar.Brand href="#home">Logo</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
+          <Nav className="m-auto">
             <Nav.Link href="#">Home</Nav.Link>
             <Nav.Link href="#">Friends</Nav.Link>
             <Nav.Link href="#">Settings</Nav.Link>
             <Nav.Link href="#">Log Out</Nav.Link>
           </Nav>
         </Navbar.Collapse>
-
+      <Navbar.Brand id="avatar">Avatar Photo</Navbar.Brand>
       </Container>
     </Navbar>
   );
