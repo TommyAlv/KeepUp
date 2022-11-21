bef32f515301f264d96d60c01e316acb19c65f7f
Alexander <alexanders.tischner@outlook.com>
2022-11-21 11:23:02 -0700

    finished postController

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -16,10 +16,39 @@ module.exports = {
     getSinglePost(req, res) {
         Post.findOne({ _id: req.params.Id })
             .select('-__v')
-            .then((post) => !post? 
+            .then((post) => !post ? 
             res.status(404).json({ message: 'No post with that ID!' })
                 : res.json(post)
                 )
                 .catch((err) => res.status(500).json(err.message));
     },
+
+    createPost(req, res) {
+        Post.create(req.body)
+            .then((post) => res.json(post))
+            .catch((err) => res.status(500).json(err));
+    },
+
+    updatePost(req, res) {
+        Post.findOneAndUpdate(req.body) 
+            .then((post) => !post ?
+                res.status(404).json({ message: 'No post with that ID!'})
+                : res.json(post)
+                )
+                .catch((err) => res.status(500).json(err.message));
+        
+    },
+
+    createComment(req, res) {
+        Post.findByIdAndUpdate(req.params.postId, { $addToSet: { comment: req.body } }, { runValidators: true, new: true })
+            .then(() => res.json({ message: 'Comment created! '}))
+            .catch((err) => res.status(500).json(err));
+
+    },
+
+    deleteComment(req, res) {
+        Post.findByIdAndUpdate(req.params.postId, { $pull: { comment: { commentId: req.params.commentId } }}, { runValidators: true, new: true })
+            .then(() => res.json({ message: 'Comment Deleted' }))
+            .catch((err) => res.status(500).json(err));
+    }
 }
