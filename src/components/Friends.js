027f0d9f225c523be90a80593b1f18666ffb109f
hrsprs <jamescharleson@yahoo.com>
2022-11-21 19:53:34 -0700

    added to friends page

--- src/components/Friends.js
+++ src/components/Friends.js
@@ -3,3 +3,66 @@
 // Remove Friend Option
 // -- RYAN
 
+import Avatar from "react-avatar"
+import { Col, Row, Container } from "react-bootstrap"
+import avatarPic from "../assets/images/Sebas-dog.jpeg"
+
+export const Friends = () => {
+    return (
+
+        <section className="vh-100">
+            <div className="page-banner">
+                <h1 className="page-title"><strong>Friends</strong>
+                </h1>
+            </div>
+            <Container>
+                <Row className="align-items-center">
+                    <Col md={2}>
+
+                    </Col>
+                    <Col md={8}>
+                        <div>
+                            <div className="friends-header">
+                                <h2>Your Friends</h2>
+                            </div>
+                            <div className="friends-box">
+                                <Container className="friends-list">
+                                        <Row className="friend-profile-item">
+                                                <Col xs lg="2">
+                                                    <Avatar className="avatar" src={avatarPic}></Avatar>
+                                                </Col>
+                                                <Col className="friend-user" md="auto">
+                                                    <strong><h3>Sebastion Tischner</h3></strong>
+                                                    <h6>@sebascrab</h6>
+                                                </Col>
+                                        </Row>
+                                        <Row className="friend-profile-item">
+                                                <Col xs lg="2">
+                                                    <Avatar className="avatar" src={avatarPic}></Avatar>
+                                                </Col>
+                                                <Col className="friend-user" md="auto">
+                                                    <strong><h3>Sebastion Tischner</h3></strong>
+                                                    <h6>@sebascrab</h6>
+                                                </Col>
+                                        </Row>
+                                        <Row className="friend-profile-item">
+                                                <Col xs lg="2">
+                                                    <Avatar className="avatar" src={avatarPic}></Avatar>
+                                                </Col>
+                                                <Col className="friend-user" md="auto">
+                                                    <strong><h3>Sebastion Tischner</h3></strong>
+                                                    <h6>@sebascrab</h6>
+                                                </Col>
+                                        </Row>
+                                </Container>
+                            </div>
+                        </div>
+                    </Col>
+                    <Col md={2}>
+
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    )
+}
